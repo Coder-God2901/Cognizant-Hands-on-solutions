82ef87fc4d7a4cb917c80817a35bed8f45f6f0de
Coder-God2901 <coderz2901@gmail.com>
2025-06-30 00:07:12 +0530

    Week-2 Solutions

---
+++ Week 2/CustomerCommunicationSolution/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailIsSentSuccessfully()
+        {
+            // Arrange
+            _mockMailSender.Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            // Act
+            var result = _customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
